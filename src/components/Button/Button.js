f125da773b729de72f08c8d4b91b8f39150fabba
Yuna <hinamoriyasmina@gmail.com>
2020-01-16 13:18:25 +0100

    Multi component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Button.scss';
+import PropTypes from 'prop-types';
 
 
-const Button = () => (<button className="button">Click me</button>);
+const Button = ({ styleName }) => {
+  const [changeColor, setChangeColor] = useState(styleName);
+
+  return (<button type="button" className={changeColor} onClick={() => setChangeColor('btn-red')}>Click me</button>);
+};
+
+Button.propTypes = {
+  styleName: PropTypes.string.isRequired,
+};
 
 export default Button;
