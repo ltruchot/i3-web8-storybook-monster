f39a63d60bc4832617e73978f391cd4e4625944b
Loïc TRUCHOT <loic.truchot.pro@gmail.com>
2020-01-16 11:46:26 +0100

    conflict sabrina

---
+++ src/components/sabrina/Sab.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Sab.scss';
+
+const Sab = ({ isText }) => (
+  <div className="card">
+    <div className="card-body myText">{isText}</div>
+  </div>
+);
+
+Sab.propTypes = {
+  isTiti: PropTypes.string.isRequired,
+};
+export default Sab;
