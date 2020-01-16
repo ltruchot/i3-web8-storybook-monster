aa01189d5d438e29b9124916a5f6d2261b98c843
Jordane <jordane.frechet@gmail.com>
2020-01-16 13:24:47 +0100

    added a kitten that's not dead

--- src/stories/jordane.stories.js
+++ src/stories/jordane.stories.js
@@ -12,18 +12,18 @@ export default {
 };
 
 export const Display = () => (
-
   <div className="this-is-fine">
     <h1>Time-Machine</h1>
     <h3>Setup : 1990s</h3>
     <div className="horror-land">
-      <Sab />
+      <Sab isText="heu yoyoyoyooooo" />
       <Toto isTiti />
       <PetAKitten tooHard />
       <Champagne openBottle />
       <Jessik isPurple />
       <ParagraphPerso green />
       <Button />
+      <PetAKitten color="black" />
     </div>
   </div>
 );
