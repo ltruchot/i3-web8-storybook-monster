95090fbb475ebe9abc674fe89f70a673fe92c045
Mel <melanie.denotte@hotmail.com>
2020-01-16 11:15:55 +0100

    add paragraph style

--- src/components/Toto.js
+++ src/components/Toto.js
@@ -1,18 +1,12 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './Toto.scss';
+import React from "react";
+import PropTypes from "prop-types";
+import "./Toto.scss";
 
 const Toto = ({ isTiti }) => (
-  <span className="toto">
-    Vive
-    {' '}
-    {isTiti ? 'titi' : 'toto' }
-    {' '}
-!!!
-  </span>
+  <span className="toto">Vive {isTiti ? "titi" : "toto"} !!!</span>
 );
 
 Toto.propTypes = {
-  isTiti: PropTypes.bool.isRequired,
+  isTiti: PropTypes.bool.isRequired
 };
 export default Toto;
