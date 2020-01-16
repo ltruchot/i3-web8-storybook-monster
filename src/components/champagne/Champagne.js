5450bb9cbd796c1819dc6b08b04c1a40b81fc6b5
Val <valentinedestrait@hotmail.com>
2020-01-16 11:34:14 +0100

    Champagne open

--- src/components/champagne/Champagne.js
+++ src/components/champagne/Champagne.js
@@ -2,12 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Champagne.scss';
 
+const clickOpenBottle = () => {
+  const image = document.getElementById('champ-img');
+  console.log(image);
+  image.src = 'open-champagne.gif';
+};
 
 const Champagne = ({ openBottle }) => (
   <div className="champagne-wrapper">
     <h1>Champagne</h1>
 
-    <img src={openBottle ? 'open-champagne.gif' : 'champagne.jpg'} alt="" />
+    {/* <img src={openBottle ? 'open-champagne.gif' : 'champagne.jpg'} alt="" /> */}
+
+    {openBottle
+      ? (
+        <>
+          <img id="champ-img" src="champagne.jpg" alt="" />
+          {' '}
+          <button type="button" onClick={clickOpenBottle}>Open</button>
+        </>
+      )
+      : <img src="champagne.jpg" alt="" />}
+
   </div>
 );
 
